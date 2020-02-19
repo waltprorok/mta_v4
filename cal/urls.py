ae92f1b44abf177c4bf0852aec861dc08514afd5
Walter Prorok <waltprorok@gmail.com>
2020-02-19 15:54:32 -0500

    updated cal files

--- cal/urls.py
+++ cal/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'cal'
+
 urlpatterns = [
-    url('calendar/', views.CalendarView.as_view(), name='calendar'),
+    url('calendar', views.CalendarView.as_view(), name='calendar'),
     url('event/new/', views.event, name='event_new'),
     url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
