1085eb9e1752d7109246ff6757abda1c03876cee
Walter Prorok <waltprorok@gmail.com>
2020-02-19 09:43:33 -0500

    added css static file

--- cal/urls.py
+++ cal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'cal'
 urlpatterns = [
-    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
-    url(r'^event/new/$', views.event, name='event_new'),
+    url('calendar/', views.CalendarView.as_view(), name='calendar'),
+    url('event/new/', views.event, name='event_new'),
     url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
